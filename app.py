9e18344894680da1679fa42ff006f84275bca75a
Zaid <itsunknown122@gmail.com>
2022-12-01 08:35:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-os.system("git clone $REPO ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anywhere
+os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
